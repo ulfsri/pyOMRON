21465ef1903494e722c4c906ff9585023ed70800
GraysonBelammy <graysontbellamy@gmail.com>
2024-03-16 17:38:58 -0400

    Update code for efficiency and clarity

--- pyOMRON/comm.py
+++ pyOMRON/comm.py
@@ -142,7 +142,9 @@ class SerialDevice(CommDevice):
         ByteString
             The serial communication.
         """
-        return await self.ser_devc.receive_some(len)
+        with trio.move_on_after(self.timeout / 1000):
+            return await self.ser_devc.receive_some(len)
+        return None
 
     async def _write(self, command: str) -> None:
         """
@@ -168,11 +170,14 @@ class SerialDevice(CommDevice):
         async with self.ser_devc:
             line = bytearray()
             while True:
+                c = None
                 with trio.move_on_after(self.timeout / 1000):
                     c = await self._read(1)
                     line += c
                     if c == self.eol:
                         break
+                if c is None:
+                    break
         return line.decode("ascii")
 
     async def _write_readall(self, command: str) -> list:
@@ -228,7 +233,7 @@ class SerialDevice(CommDevice):
                     line += c
                 if c is None:
                     break
-            return line
+        return line
 
     async def _flush(self) -> None:
         """
