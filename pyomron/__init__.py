f1779157d8c01cfe8da057673da0166d0499e5e5
GraysonBellamy <grayson.bellamy@ul.org>
2024-04-06 10:00:46 -0400

    Code formatting

--- pyomron/__init__.py
+++ pyomron/__init__.py
@@ -3,10 +3,7 @@
 
 import sys
 
-if sys.version_info >= (3, 8):
-    from importlib import metadata as importlib_metadata
-else:
-    import importlib_metadata
+from importlib import metadata as importlib_metadata
 
 
 def get_version() -> str:
